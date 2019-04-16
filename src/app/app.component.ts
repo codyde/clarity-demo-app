452accfd3b4a854ae1a8b9b882757c59680064da
Cody De Arkland <codydearkland@gmail.com>
2019-04-16 00:32:41 -0700

    Adding sample project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+export interface Person {
+  name: string
+  blogurl: string
+  twitter: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'clarity-demo-app';
+  
+  people: Person[] = [
+    {name: 'Cody De Arkland', blogurl: 'https://www.thehumblelab.com', twitter: "@CodyDeArkland"},
+    {name: 'Grant Orchard', blogurl: 'https://www.grantorchard.com', twitter: "@GrantOrchard"},
+    {name: 'Jon Schulman', blogurl: 'http://www.vaficionado.com', twitter: "@vAficionado"},
+    {name: 'Jad El-Zein', blogurl: 'https://www.virtualjad.com', twitter: "@virtualjad"},
+    {name: 'Tony Reeves', blogurl: 'https://www.digitalvspace.com', twitter: "@importcarguy"},
+    {name: 'Al Rasheed', blogurl: 'https://alarasheedblog.wordpress.com/', twitter: "@al_rasheed"}
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ClarityModule } from "@clr/angular";
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ClarityModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
